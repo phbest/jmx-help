40c4c27b7642eec5d939c813da3f12d2df4eeed8
Philip Best <philipbest@Philips-MacBook-Pro.local>
2016-05-15 09:04:10 -0500

    Project init

---
+++ jmx-help/src/jmx_help/core.clj
@@ -0,0 +1,7 @@
+(ns jmx-help.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ jmx-help/test/jmx_help/core_test.clj
@@ -0,0 +1,7 @@
+(ns jmx-help.core-test
+  (:require [clojure.test :refer :all]
+            [jmx-help.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
